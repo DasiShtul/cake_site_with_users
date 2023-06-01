15c06c913907911c51d676f0c4b0ff9681ecc082
DasiShtul <shtull61@gmeil.com>
2023-06-01 14:09:36 +0300

    first 2

--- routes/config_routes.js
+++ routes/config_routes.js
@@ -1,11 +1,13 @@
 const indexR= require("./index");
 const usersR= require("./users");
-const cakesR= require("./cakes");
+// const cakesR= require("./cakes");
+const countryR= require("./country");
 
 
 exports.routesInit =(app)=>{
     app.use("/",indexR)
     app.use("/users",usersR)
-   app.use("/cakes",cakesR)
+//    app.use("/cakes",cakesR)
+   app.use("/country",countryR)
   
 }
